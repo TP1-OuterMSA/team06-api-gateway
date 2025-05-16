8574d4910fa328ba7d50ac0b417717f1c12978ff
yeilkk <yeil99@naver.com>
2025-05-16 17:16:05 +0900

    feat: gateway local

---
+++ src/main/java/com/example/team06apigateway/Team06ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.team06apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team06ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team06ApiGatewayApplication.class, args);
+    }
+
+}
