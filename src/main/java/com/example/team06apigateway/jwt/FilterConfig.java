6eaaec018bc48d47969ee71a2d0e79a56738d26f
yeilkk <yeil99@naver.com>
2025-05-21 16:21:23 +0900

    chore: 브랜치 merge

---
+++ src/main/java/com/example/team06apigateway/jwt/FilterConfig.java
@@ -0,0 +1,13 @@
+package com.example.team06apigateway.jwt;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public JwtAuthenticationGatewayFilterFactory jwtAuthenticationFilter(JwtConfigProperties jwtConfigProperties) {
+        return new JwtAuthenticationGatewayFilterFactory(jwtConfigProperties);
+    }
+}
